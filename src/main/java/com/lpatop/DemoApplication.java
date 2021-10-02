e60eda39d3f9be538653be8b2b77daf3f2620a81
0026U6744 <0026U6744@IN0026U6>
2021-10-02 11:37:58 +0530

    oneToMany (one customer many address)

--- src/main/java/com/lpatop/DemoApplication.java
+++ src/main/java/com/lpatop/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.lpatop;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lpatop.model.Address;
+import com.lpatop.model.Customer;
 import com.lpatop.model.Laptop;
 import com.lpatop.model.StorageType;
+import com.lpatop.repository.AddressRepository;
+import com.lpatop.repository.CustomerRepository;
 import com.lpatop.repository.LaptopRepo;
 import com.lpatop.repository.StorageTypeRepo;
 
@@ -17,41 +20,69 @@ import com.lpatop.repository.StorageTypeRepo;
 public class DemoApplication implements CommandLineRunner {
 
 	@Autowired
-	LaptopRepo laptopRepo;
+	private LaptopRepo laptopRepo;
 	@Autowired
-	StorageTypeRepo storageTypeRepo;
-	
+	private StorageTypeRepo storageTypeRepo;
+
+	@Autowired
+	private CustomerRepository customerRepo;
+
+	@Autowired
+	private AddressRepository addressRepo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+//****************************************************************
+		// Adding storage type and laptops
 		StorageType ssd512 = new StorageType("ssd", 512);
 		StorageType hdd = new StorageType("hdd", 1024);
 		StorageType ssd256 = new StorageType("ssd", 256);
 		storageTypeRepo.save(ssd512);
 		storageTypeRepo.save(hdd);
 		storageTypeRepo.save(ssd256);
-		
+
 		Laptop dellI5 = new Laptop("Di5", "i5", 8);
 		dellI5.getStorageType().add(ssd512);
-		
+
 		Laptop dellI7 = new Laptop("Di7", "i7", 16);
 		dellI7.getStorageType().add(ssd256);
-		
+
 		Laptop dellI3 = new Laptop("Di3", "i3", 4);
-		dellI3.getStorageType().add(hdd);		
+		dellI3.getStorageType().add(hdd);
 		Laptop dellI5Hybrid = new Laptop("Di7", "i7", 16);
 		dellI5Hybrid.getStorageType().add(ssd256);
 		dellI5Hybrid.getStorageType().add(hdd);
-		
-		
+
 		laptopRepo.save(dellI5);
 		laptopRepo.save(dellI7);
 		laptopRepo.save(dellI3);
 		laptopRepo.save(dellI5Hybrid);
-		
+
+//*************************************************************************
+		// Adding Address and customer
+		Customer customer = new Customer("Manoj", "Kanasugara", "manojkanasugara06@gmail.com", "7676545212");
+
+		Address primaryAddress = new Address("India", "Karnataka", "Bidar", "Bidar", "Bidar", 585421, "near temple");
+		primaryAddress.setCustomer(customer);
+		Address secondaryAddress = new Address("India", "Karnataka", "Bidar", "Bidar", "Manhalli", 585403,
+				"near river");
+		secondaryAddress.setCustomer(customer);
+		Address officeAddress = new Address("India", "Karnataka", "Bengalur", "Bengaluru", "Namma Bengaluru", 560067,
+				"near college");
+		officeAddress.setCustomer(customer);
+
+		customer.getAddressList().add(primaryAddress);
+		customer.getAddressList().add(secondaryAddress);
+		customer.getAddressList().add(officeAddress);
+		customerRepo.save(customer);
+
+		addressRepo.save(primaryAddress);
+		addressRepo.save(secondaryAddress);
+		addressRepo.save(officeAddress);
 
 	}
 
--- src/main/java/com/lpatop/service/LaptopServieImpl.java
+++ src/main/java/com/lpatop/service/LaptopServieImpl.java
@@ -1,6 +1,11 @@
 package com.lpatop.service;
 
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -20,10 +25,22 @@ public class LaptopServieImpl implements LaptopService {
 		// TODO Auto-generated method stub
 		return laptopRepo.findByProcessor(processor);
 	}
+
 	@Override
-	public List<Laptop> getLaptopByProcessorAndRam(String processor,int ram) {
-		// TODO Auto-generated method stub
-		return laptopRepo.findByProcessorAndRam(processor,ram);
+	public List<Laptop> getLaptopByProcessorAndRam(String processor, int ram) {
+		System.out.println("inside service");
+		List<Laptop> l = laptopRepo.findByProcessorAndRam(processor, ram);
+
+		Map<Integer, Laptop> e = l.stream().collect(Collectors.toMap(x -> x.getRam(), Function.identity()));
+		// .forEach(x -> System.out.println(x.getKey() + "--" +
+		// x.getValue().getProcessor()));
+//e.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey)).
+//collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(e1,e2)->e2,LinkedHashMap::new));
+
+		e.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+		return l;
 	}
 
 }
